994b860b3f00e0a64694d2ec61425bd64c3ca6ce
Edoardo Ursi <s286346@studenti.polito.it>
2025-05-07 10:11:39 +0200

    Sentinel_Death

--- ProgettoGameDesign/Assets/Scripts/Enemies/Sentinel/Sentinel_Script.cs
+++ ProgettoGameDesign/Assets/Scripts/Enemies/Sentinel/Sentinel_Script.cs
@@ -18,4 +18,14 @@ public class Sentinel : Enemy
         Vector2 projectileDirection = (player.transform.position - projectileTransform.position).normalized;
         projectile.GetComponent<Rigidbody2D>().AddForce(projectileDirection * projectileSpeed, ForceMode2D.Impulse);
     }
+    public override void EnemyHit(float damage, Vector2 hitDirection, float _hitForce)
+    {
+        base.EnemyHit(damage, hitDirection, _hitForce);
+
+        if (health <= 0)
+        {
+            Death(1.5f); // Oppure ChangeState(EnemyStates.Sentinel_Death) se ne definisci uno
+        }
+    }
+
 }
