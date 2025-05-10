5a7cdc4a857806ecdb002a0e92cbf65a3cb4f2c6
federicocolombo12 <chiccoclub5@gmail.com>
2025-05-10 12:01:49 +0200

    Boss Death

---
+++ ProgettoGameDesign/Assets/Scripts/AI/Tasks/DestroyBoss.cs
@@ -0,0 +1,33 @@
+using BehaviorDesigner.Runtime.Tasks;
+
+using DG.Tweening;
+using TMPro.Examples;
+using UnityEngine;
+
+
+
+    public class DestroyBoss : EnemyAction
+    {
+        public float bleedTime = 2.0f;
+        public ParticleSystem bleedEffect;
+        public ParticleSystem explodeEffect;
+
+        private bool isDestroyed;
+
+        public override void OnStart()
+        {
+            EffectManager.Instance.PlayOneShot(bleedEffect, transform.position);
+            DOVirtual.DelayedCall(bleedTime, () =>
+            {
+                EffectManager.Instance.PlayOneShot(explodeEffect, transform.position);
+                CameraManager.Instance.ShakeCamera(0.7f);
+                isDestroyed = true;
+                Object.Destroy(gameObject);
+            }, false);
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            return isDestroyed ? TaskStatus.Success : TaskStatus.Running;
+        }
+    }
