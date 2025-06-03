39b7d4a5a76b5adea7512bd8772b4b4389b5de7e
federicocolombo12 <126761702+federicocolombo12@users.noreply.github.com>
2025-06-03 21:23:28 +0200

    Dash

--- ProgettoGameDesign/Assets/Scripts/Player/PlayerDash.cs
+++ ProgettoGameDesign/Assets/Scripts/Player/PlayerDash.cs
@@ -35,14 +35,15 @@ public class PlayerDash : MonoBehaviour
         rb = Player.Instance.rb;
         animator = Player.Instance.animator;
         playerMovement = Player.Instance.playerMovement;
-        
+
     }
     void ChangeVariables()
     {
         animator = Player.Instance.animator;
     }
 
-    public void DoDash(bool dashed) { 
+    public void DoDash(bool dashed)
+    {
         if (canDash && !pState.dashing && dashed)
         {
             StartCoroutine(Dash());
@@ -55,41 +56,53 @@ public class PlayerDash : MonoBehaviour
         canDash = false;
         pState.dashing = true;
         animator.SetTrigger("Dashing");
-        
-       
-        
-        while (rb.gravityScale !=0){
-                rb.gravityScale = 0;
-                yield return null;
+
+
+
+        while (rb.gravityScale != 0)
+        {
+            rb.gravityScale = 0;
+            yield return null;
         }
-        
+
         rb.linearVelocity = new Vector2(rb.linearVelocity.x * dashSpeed, 0);
-        
-        
-        EffectManager.Instance.PlayOneShot(dashEffect.GetComponent<ParticleSystem>(), transform.position+Vector3.down*effectPosition);
+
+
+        EffectManager.Instance.PlayOneShot(dashEffect.GetComponent<ParticleSystem>(), transform.position + Vector3.down * effectPosition);
         ChangeAlpha();
+        ParticleSpawn();
+
         CameraManager.Instance.ShakeCamera(shakeIntensity);
         yield return new WaitForSeconds(dashTime);
-        
+
         pState.dashing = false;
-        rb.gravityScale =   playerMovement.gravityScale;
-        
-        
+        rb.gravityScale = playerMovement.gravityScale;
+
+
         yield return new WaitForSeconds(dashCooldown);
-       
+
         canDash = true;
-        
-        
+
+
     }
     void ChangeAlpha()
     {
         foreach (SpriteRenderer spriteRenderer in GetComponentsInChildren<SpriteRenderer>())
         {
-            spriteRenderer.DOFade(0, dashTime).OnComplete(() => 
+            spriteRenderer.DOColor(Color.black, 0.1f).OnComplete(() =>
             {
-                spriteRenderer.DOFade(1, dashCooldown);
+                spriteRenderer.DOColor(Color.white, 0.1f);
             });
         }
-        
+
+    }
+    void ParticleSpawn()
+    {
+        if (dashTrail != null)
+        {
+            GameObject trail = Instantiate(dashTrail, dashPosition.position, Quaternion.identity, dashPosition);
+            
+            Destroy(trail, dashCooldown);
+        }
     }
 }
