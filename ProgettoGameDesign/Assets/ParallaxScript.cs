b7f0d3222b59bc225787d0dc66bb95ab33dead2f
Edoardo Ursi <s286346@studenti.polito.it>
2025-06-12 12:05:54 +0200

    verticale orizzontale parallasse

--- ProgettoGameDesign/Assets/ParallaxScript.cs
+++ ProgettoGameDesign/Assets/ParallaxScript.cs
@@ -2,18 +2,33 @@ using UnityEngine;
 
 public class ParallaxScript : MonoBehaviour
 {
+    [Header("Fattore di parallasse ")]
     public float parallaxFactor = 0.5f;
+
+    [Header("Assi abilitati")]
+    public bool enableHorizontal = true;
+    public bool enableVertical = false;
+
     private Vector3 lastCameraPosition;
+    private Camera cam;
 
     void Start()
     {
-        lastCameraPosition = Camera.main.transform.position;
+        cam = Camera.main;
+        if (cam != null)
+            lastCameraPosition = cam.transform.position;
     }
 
     void Update()
     {
-        Vector3 delta = Camera.main.transform.position - lastCameraPosition;
-        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
-        lastCameraPosition = Camera.main.transform.position;
+        if (cam == null) return;
+
+        Vector3 delta = cam.transform.position - lastCameraPosition;
+
+        float deltaX = enableHorizontal ? delta.x * parallaxFactor : 0f;
+        float deltaY = enableVertical ? delta.y * parallaxFactor : 0f;
+
+        transform.position += new Vector3(deltaX, deltaY, 0);
+        lastCameraPosition = cam.transform.position;
     }
 }
