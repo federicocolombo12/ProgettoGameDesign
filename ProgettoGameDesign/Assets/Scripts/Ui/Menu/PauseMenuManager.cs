fe997687c60a53c805efbee89910e41a42deb1b9
federicocolombo12 <chiccoclub5@gmail.com>
2025-06-18 17:50:10 +0200

    Pause Menu

--- ProgettoGameDesign/Assets/Scripts/Ui/Menu/PauseMenuManager.cs
+++ ProgettoGameDesign/Assets/Scripts/Ui/Menu/PauseMenuManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
+using DG.Tweening;
 
 
 public class PauseMenuManager : MonoBehaviour
@@ -8,11 +9,14 @@ public class PauseMenuManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     Canvas pauseMenuCanvas;
+    CanvasGroup pauseMenuCanvasGroup;
     [SerializeField] InputActionReference quitActionReference;
+    [SerializeField] float targetAlpha = 0.8f; // Opacit�� finale del menu di pausa
     private void Start()
     {
         pauseMenuCanvas = GetComponent<Canvas>();
-        pauseMenuCanvas.enabled = false; // Disabilita il menu di pausa all'inizio
+        pauseMenuCanvas.enabled = false; // Disabilita il menu di pausa all'inizio+
+        pauseMenuCanvasGroup = GetComponent<CanvasGroup>();
     }
     private void OnEnable()
     {
@@ -43,11 +47,12 @@ public class PauseMenuManager : MonoBehaviour
     {
 
         pauseMenuCanvas.enabled = true; // Abilita il menu di pausa quando l'input � attivo
+        pauseMenuCanvasGroup.alpha = targetAlpha; // Imposta l'opacit�� iniziale a 0
         Time.timeScale= 0f; // Ferma il tempo di gioco
     }   
     void DisableMenu(InputAction.CallbackContext callback)
     {
-        
+        pauseMenuCanvasGroup.alpha = 0f; // Imposta l'opacit�� a 0 per nascondere il menu di pausa
         Canvas pauseMenuCanvas = GetComponent<Canvas>();
         if (pauseMenuCanvas == null)
         {
