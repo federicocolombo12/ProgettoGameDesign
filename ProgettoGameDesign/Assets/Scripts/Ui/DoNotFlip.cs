eea69cad3b9621026d8a081a89ba46d978057130
federicocolombo12 <chiccoclub5@gmail.com>
2025-06-19 19:44:42 +0200

    Update DoNotFlip.cs

--- ProgettoGameDesign/Assets/Scripts/Ui/DoNotFlip.cs
+++ ProgettoGameDesign/Assets/Scripts/Ui/DoNotFlip.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class DoNotFlip : MonoBehaviour
 {
     [SerializeField] RectTransform rectTransform;
+    Vector3 initialScale;
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        initialScale = rectTransform.localScale;
+
     }
     void LateUpdate()
 {
-    Vector3 parentScale = transform.parent.localScale;
-
-        rectTransform.localScale = parentScale.x > 0 ? new Vector3(rectTransform.localScale.x, rectTransform.localScale.y, rectTransform.localScale.z) : new Vector3(-rectTransform.localScale.x, rectTransform.localScale.y, rectTransform.localScale.z);
+    
+        Vector3 parentScale = rectTransform.parent.localScale;
+        rectTransform.localScale = parentScale.x > 0 ? new Vector3(initialScale.x, initialScale.y, initialScale.z) 
+            : new Vector3(-initialScale.x, initialScale.y, initialScale.z);
     }
 }
