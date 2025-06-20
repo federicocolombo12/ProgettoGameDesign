97daa8b0863c6bf8431652a2e0346695a7445fd2
federicocolombo12 <chiccoclub5@gmail.com>
2025-06-20 12:46:40 +0200

    Canvas trasformazione

---
+++ ProgettoGameDesign/Assets/MainTrasformCooldownHandler.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainTrasformCooldownHandler : MonoBehaviour
+{
+    Image mainImage;
+    private void Start()
+    {
+        mainImage = GetComponent<Image>();
+    }
+    private void OnEnable()
+    {
+        PlayerTransform.OnTransformEffect += HandleTransformCooldown;
+    }
+
+    private void HandleTransformCooldown(float timer, Color color)
+    {
+        mainImage.fillAmount = 0f;
+        //lerp the value with do tween to 1
+        mainImage.DOFillAmount(1f, timer).SetEase(Ease.Linear);
+    }
+}
